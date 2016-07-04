22bd4e2a7e3d03813b375d0bfcaf651c897f5ea2
Ian Sinnott <ian@iansinnott.com>
2016-07-03 23:19:51 -0700

    Update example to no longer use any special logic in index file
    Now that we can pass routes directly it's no longer necessary to differentiate
    between the server and the client in the index file if the routes are in a
    separate file. Note however that if they are in the same file this will still
    be necessary.

--- example/src/index.js
+++ example/src/index.js
@@ -5,14 +5,7 @@ import { Router, browserHistory } from 'react-router';
 // Import your routes so that you can pass them to the <Router /> component
 import routes from './routes.js';
 
-// Only render in the browser
-if (typeof document !== 'undefined') {
-  render(
-    <Router routes={routes} history={browserHistory} />,
-    document.getElementById('root')
-  );
-}
-
-// Export the routes here so that ReactStaticPlugin can access them and build
-// the static files.
-export * from './routes.js';
+render(
+  <Router routes={routes} history={browserHistory} />,
+  document.getElementById('root')
+);
