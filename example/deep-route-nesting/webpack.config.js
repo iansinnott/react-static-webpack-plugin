fe64f45d29e2a74326a8ebce6ef845b3b44d5677
Ian Sinnott <ian@iansinnott.com>
2016-07-08 17:02:20 -0700

    Add two more tests

---
+++ example/deep-route-nesting/webpack.config.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-var */
+var path = require('path');
+
+var ReactStaticPlugin = require('../../dist');
+
+module.exports = {
+  devtool: 'source-map',
+
+  entry: {
+    app: './src/index.js',
+  },
+
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: '[name].js',
+    libraryTarget: 'umd',
+    publicPath: '/',
+  },
+
+  plugins: [
+    new ReactStaticPlugin({
+      routes: './src/routes.js',
+    }),
+  ],
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel'],
+        include: path.join(__dirname, 'src'),
+      },
+    ],
+  },
+};
