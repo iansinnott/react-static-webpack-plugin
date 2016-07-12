2bace015ade0b94aa80d112bacf383f17056afc7
Ian Sinnott <ian@iansinnott.com>
2016-07-12 00:11:02 -0700

    Minor code cleanup

--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,3 @@
-
-// TODO: Should this be hard coded?
-export const ROUTES_FILENAME = 'routes.js';
-export const outputFilenames = {
-
-};
-
 export type OptionsShape = {
   routes: string,
   template?: string,
