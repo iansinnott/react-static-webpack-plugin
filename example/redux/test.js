390ff8f3ba0755689fcb73045ef7ab130939aa90
Ian Sinnott <ian@iansinnott.com>
2016-07-13 15:33:01 -0700

    Fix issue with redux test

--- example/redux/test.js
+++ example/redux/test.js
@@ -5,7 +5,7 @@ import path from 'path';
 
 import options from './webpack.config.prod.js';
 
-test.cb('Supports minification as well as CSS modules', t => {
+test.cb('Supports redux apps that must be wrapped in <Provider>', t => {
   webpack(options, function(err, stats) {
     if (err) {
       return t.end(err);
@@ -20,7 +20,7 @@ test.cb('Supports minification as well as CSS modules', t => {
     t.true(files.includes('who.html'));
     t.true(files.includes('how.html'));
     t.true(files.includes('log-in.html'));
-    t.true(files.includes('sig-up.html'));
+    t.true(files.includes('sign-up.html'));
     t.true(files.includes('404.html'));
 
     t.true(files.includes('app.css'));
@@ -29,7 +29,7 @@ test.cb('Supports minification as well as CSS modules', t => {
     // Test size in MB. We want to make sure this bundle was minified since we
     // are using the minify JS plugin
     const bundle = assets[files.indexOf('app.js')];
-    t.true((bundle.size / 1000) < 300);
+    t.true((bundle.size / 1000) < 500);
 
     t.end();
   });
