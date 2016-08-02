ce08be08c1754ccb951a89fc4687e1d52be04f1e
Ian Sinnott <ian@iansinnott.com>
2016-08-02 14:07:56 -0700

    Fix titles. Put them on indexroute instead of top-most route

--- example/basic-with-router/src/routes.js
+++ example/basic-with-router/src/routes.js
@@ -4,8 +4,8 @@ import { Route, IndexRoute } from 'react-router';
 import { App, Home, About, NotFound } from './components.js';
 
 export const routes = (
-  <Route path='/' title='App' component={App}>
-    <IndexRoute component={Home} />
+  <Route path='/' component={App}>
+    <IndexRoute title='App' component={Home} />
     <Route path='about' title='App - About' component={About} />
     <Route path='*' title='404: Not Found' component={NotFound} />
   </Route>
