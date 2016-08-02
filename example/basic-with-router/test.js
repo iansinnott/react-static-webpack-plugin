08b562e335e7340ac16b0e452643e7404211914a
Ian Sinnott <ian@iansinnott.com>
2016-08-02 14:08:59 -0700

    Update tests and separate where necessary

--- example/basic-with-router/test.js
+++ example/basic-with-router/test.js
@@ -1,22 +1,41 @@
 import test from 'ava';
 import webpack from 'webpack';
+import fs from 'fs';
+import path from 'path';
 
 import options from './webpack.config.js';
 
-test.cb('Compiles routes nested at one level', t => {
-  webpack(options, function(err, stats) {
+let stats;
+
+test.cb.before(t => {
+  webpack(options, (err, _stats) => {
     if (err) {
       return t.end(err);
-    } else if (stats.hasErrors()) {
-      return t.end(stats.toString());
+    } else if (_stats.hasErrors()) {
+      return t.end(_stats.toString());
     }
 
-    const files = stats.toJson().assets.map(x => x.name);
-
-    t.true(files.includes('index.html'));
-    t.true(files.includes('about.html'));
-    t.true(files.includes('404.html'));
+    stats = _stats;
 
     t.end();
   });
+});
+
+test.cb('Compiles routes nested at one level', t => {
+  const files = stats.toJson().assets.map(x => x.name);
+
+  t.true(files.includes('index.html'));
+  t.true(files.includes('about.html'));
+  t.true(files.includes('404.html'));
+
+  t.end();
+});
+
+test.cb('Output files contain titles specified in routes file', t => {
+  const outputFilepath = path.join(options.output.path, 'index.html');
+  const outputFileContents = fs.readFileSync(outputFilepath, { encoding: 'utf8' });
+
+  t.true(outputFileContents.includes('<title>App</title>'));
+
+  t.end();
 });
--- example/css-modules/test.js
+++ example/css-modules/test.js
@@ -5,33 +5,55 @@ import path from 'path';
 
 import options from './webpack.config.prod.js';
 
-test.cb('Supports minification as well as CSS modules', t => {
-  webpack(options, function(err, stats) {
+let stats;
+
+test.cb.before(t => {
+  webpack(options, (err, _stats) => {
     if (err) {
       return t.end(err);
-    } else if (stats.hasErrors()) {
-      return t.end(stats.toString());
+    } else if (_stats.hasErrors()) {
+      return t.end(_stats.toString());
     }
 
-    const { assets } = stats.toJson();
-    const files = assets.map(x => x.name);
-
-    t.true(files.includes('index.html'));
-    t.true(files.includes('about.html'));
-    t.true(files.includes('404.html'));
-    t.true(files.includes('app.css'));
-    t.true(files.includes('app.js'));
-
-    // Test CSS module compilation. Plain text classname should not be present
-    const outputFilepath = path.join(options.output.path, 'index.html');
-    const outputFileContents = fs.readFileSync(outputFilepath, { encoding: 'utf8' });
-    t.false(outputFileContents.includes('testableModuleClassName'));
-
-    // Test size in MB. We want to make sure this bundle was minified since we
-    // are using the minify JS plugin
-    const bundle = assets[files.indexOf('app.js')];
-    t.true((bundle.size / 1000) < 300);
+    stats = _stats;
 
     t.end();
   });
+});
+
+test.cb('Outputs the desired files', t => {
+  const { assets } = stats.toJson();
+  const files = assets.map(x => x.name);
+
+  t.true(files.includes('index.html'));
+  t.true(files.includes('about.html'));
+  t.true(files.includes('404.html'));
+  t.true(files.includes('app.css'));
+  t.true(files.includes('app.js'));
+
+  t.end();
+});
+
+test.cb('Compiles local CSS classes (CSS Modules)', t => {
+  const { assets } = stats.toJson();
+  const files = assets.map(x => x.name);
+  const outputFilepath = path.join(options.output.path, 'index.html');
+  const outputFileContents = fs.readFileSync(outputFilepath, { encoding: 'utf8' });
+
+  // Simply make sure this classname isn't found
+  t.false(outputFileContents.includes('testableModuleClassName'));
+
+  t.end();
+});
+
+test.cb('Supports minification', t => {
+  const { assets } = stats.toJson();
+  const files = assets.map(x => x.name);
+  const bundle = assets[files.indexOf('app.js')];
+
+  // Test size in MB. We want to make sure this bundle was minified since we
+  // are using the minify JS plugin
+  t.true((bundle.size / 1000) < 300);
+
+  t.end();
 });
