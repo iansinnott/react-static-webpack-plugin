984d46b599ebd3861a3d32affd6cda91b9702a5d
Ian Sinnott <ian@iansinnott.com>
2016-11-14 21:52:41 -0800

    Update example to generate index.html files

--- example/deep-route-nesting/src/routes.js
+++ example/deep-route-nesting/src/routes.js
@@ -13,12 +13,12 @@ export const routes = (
   <Route path='/' title='App' component={App}>
     <IndexRoute component={Home} />
     <Route path='products' title='App - Products' component={Products}>
-      <Route path='first' title='App - Products - First' component={Product} />
-      <Route path='second' title='App - Products - Second' component={Product} />
+      <Route path='first/' title='App - Products - First' component={Product} />
+      <Route path='second/' title='App - Products - Second' component={Product} />
       <Route path='third' title='App - Products - Third' component={Product}>
         <Route path='colors' title='App - Products - Third - Colors' component={ProductColors}>
-          <Route path='green' title='App - Products - Third - Colors - Green' component={ProductColor} />
-          <Route path='blue' title='App - Products - Third - Colors - Blue' component={ProductColor} />
+          <Route path='green/' title='App - Products - Third - Colors - Green' component={ProductColor} />
+          <Route path='blue/' title='App - Products - Third - Colors - Blue' component={ProductColor} />
         </Route>
       </Route>
     </Route>
