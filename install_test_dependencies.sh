4a71e6c7ba136dacbe7802b2a8b1a375c8290343
Ian Sinnott <ian@iansinnott.com>
2016-12-09 15:16:09 -0800

    Install deps in with-auth directory

--- install_test_dependencies.sh
+++ install_test_dependencies.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 echo "Installing test dependencies..."
-echo "1/6"
+echo "1/7"
 (cd example/basic-with-router; npm install &> /dev/null)
-echo "2/6"
+echo "2/7"
 (cd example/css-modules; npm install &> /dev/null)
-echo "3/6"
+echo "3/7"
 (cd example/deep-route-nesting; npm install &> /dev/null)
-echo "4/6"
+echo "4/7"
 (cd example/extract-css; npm install &> /dev/null)
-echo "5/6"
+echo "5/7"
 (cd example/with-template; npm install &> /dev/null)
-echo "6/6"
+echo "6/7"
+(cd example/with-auth; npm install &> /dev/null)
+echo "7/7"
 (cd example/redux; npm install &> /dev/null)
 echo "Finished installing test all dependencies."
