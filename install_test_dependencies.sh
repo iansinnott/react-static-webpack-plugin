edc8136eb6258f10271f41b4449279ded5c98779
Ian Sinnott <ian@iansinnott.com>
2016-12-28 12:39:34 -0800

    Update test installation script

--- install_test_dependencies.sh
+++ install_test_dependencies.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 echo "Installing test dependencies..."
-echo "1/9"
+echo "1/10"
 (cd example/basic-with-router; npm install &> /dev/null)
-echo "2/9"
+echo "2/10"
 (cd example/css-modules; npm install &> /dev/null)
-echo "3/9"
+echo "3/10"
 (cd example/deep-route-nesting; npm install &> /dev/null)
-echo "4/9"
+echo "4/10"
 (cd example/extract-css; npm install &> /dev/null)
-echo "5/9"
+echo "5/10"
 (cd example/with-template; npm install &> /dev/null)
-echo "6/9"
+echo "6/10"
 (cd example/with-mock-auth; npm install &> /dev/null)
-echo "7/9"
+echo "7/10"
 (cd example/redux; npm install &> /dev/null)
-echo "8/9"
+echo "8/10"
 (cd example/with-static-markup; npm install &> /dev/null)
-echo "9/9"
+echo "9/10"
 (cd example/with-auth0; npm install &> /dev/null)
+echo "10/10"
+(cd example/webpack-2; npm install &> /dev/null)
 echo "Finished installing test all dependencies."
