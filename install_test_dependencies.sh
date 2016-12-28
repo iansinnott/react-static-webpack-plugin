fd5a1abbc4eff81eda544976ee7cce0ca4363ef2
Ian Sinnott <ian@iansinnott.com>
2016-12-28 15:39:07 -0800

    Add yarn support for CI

--- install_test_dependencies.sh
+++ install_test_dependencies.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 echo "Installing test dependencies..."
 echo "0/10"
-(cd example/basic-with-router; npm install &> /dev/null)
+(cd example/basic-with-router; yarn install &> /dev/null)
 echo "1/10"
-(cd example/css-modules; npm install &> /dev/null)
+(cd example/css-modules; yarn install &> /dev/null)
 echo "2/10"
-(cd example/deep-route-nesting; npm install &> /dev/null)
+(cd example/deep-route-nesting; yarn install &> /dev/null)
 echo "3/10"
-(cd example/extract-css; npm install &> /dev/null)
+(cd example/extract-css; yarn install &> /dev/null)
 echo "4/10"
-(cd example/with-template; npm install &> /dev/null)
+(cd example/with-template; yarn install &> /dev/null)
 echo "5/10"
-(cd example/with-mock-auth; npm install &> /dev/null)
+(cd example/with-mock-auth; yarn install &> /dev/null)
 echo "6/10"
-(cd example/redux; npm install &> /dev/null)
+(cd example/redux; yarn install &> /dev/null)
 echo "7/10"
-(cd example/with-static-markup; npm install &> /dev/null)
+(cd example/with-static-markup; yarn install &> /dev/null)
 echo "8/10"
-(cd example/with-auth0; npm install &> /dev/null)
+(cd example/with-auth0; yarn install &> /dev/null)
 echo "9/10"
-(cd example/webpack-2; npm install &> /dev/null)
+(cd example/webpack-2; yarn install &> /dev/null)
 echo "10/10. Finished installing test all dependencies."
